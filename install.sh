e07b105cc86d3e63ef7807163625fd8134d94be1
David Smerkous <smerkousdavid@gmail.com>
2015-09-23 21:39:26 -0500

    Create install.sh
    I looked at chromebrew to get an idea, obviously everything will be more efficient than crew later on

---
+++ install.sh
@@ -0,0 +1,19 @@
+#
+# Welcome to Crevos which is something short for Chrome Development Operating System
+# If you need to change the directories for the files below just do so
+# If you would like to contact Eli: <Email> or David: smerkousdavid@gmail.com
+# We would be happy to help (P.S. some of this you might see is similar to chromebrew)
+
+# Directories for all files
+CREVOSCREATORS="David Smerkous and Eli Smith"
+CREVOSMODIFIERS="NONE"
+URLTOCREVOS="https://raw.github.com/smerkousdavid/Crevos/master"
+CREVOS_PRE=/usr/local
+CREVOS_LIB_PATH=$CREVOS_PRE/lib/crevos/
+CREVOS_CONFIG_PATH=$CREVOS_PRE/etc/crevos/
+CREVOS_BREW_DIR=$CREVOS_PRE/tmp/crevos/
+CREVOS_DEST_DIR=$CREVOS_BREW_DIR/dest
+CREVOS_PACKAGES_PATH=$CREVOS_LIB_PATH/packages
+
+#Similar to chromebrew check to see if architecture is similar
+
