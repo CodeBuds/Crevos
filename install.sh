2533f67d67a16a38c7133ba553755481334c17c8
David Smerkous <smerkousdavid@gmail.com>
2015-09-24 12:45:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -140,6 +140,6 @@ git config core.sparsecheckout true
 echo packages >> .git/info/sparse-checkout
 echo lib >> .git/info/sparse-checkout
 echo crevos >> .git/info/sparse-checkout
-git fetch origin master
+git fetch origin master 
 git reset --hard origin/master
 echo "Crevos has been installed (ALPHA TO THE BETA)...\nSo see if you can run something graphical"
