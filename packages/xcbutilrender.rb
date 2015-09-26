cde5ac590b8492b1ded98c4be998273e23093fee
David Smerkous <smerkousdavid@gmail.com>
2015-09-26 16:08:49 -0500

    Create xcbutilrender.rb

---
+++ packages/xcbutilrender.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Xcbutilrender < Package
+  version '0.1.1'
+  source_url 'http://xcb.freedesktop.org/dist/xcb-util-keysyms-0.4.0.tar.bz2'
+  source_sha1 '1022293083eec9e62d5659261c29e367'
+
+  depends_on 'buildessential'
+
+  def self.build
+    system "./configure $XORG_CONFIG"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
