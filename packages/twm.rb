074a1f428bb8d2818c4921b4991034647ee1b649
David Smerkous <smerkousdavid@gmail.com>
2015-09-26 18:37:41 -0500

    Update twm.rb

--- packages/twm.rb
+++ packages/twm.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class Graphicsmagick < Package
+class Twm < Package
   version '1.3.21'
   source_url 'ftp://ftp.x.org/pub/individual/app/twm-1.0.9.tar.bz2'
   source_sha1 '59a6f076cdacb5f6945dac809bcf4906'
