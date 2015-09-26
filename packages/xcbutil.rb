a78963bf18c06d1689a82c2a499af24103ff3fa2
David Smerkous <smerkousdavid@gmail.com>
2015-09-26 16:02:19 -0500

    Create xcbutil.rb

---
+++ packages/xcbutil.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Xcbutil < Package
+  version '0.1.1'
+  source_url 'http://xcb.freedesktop.org/dist/xcb-util-0.4.0.tar.bz2'
+  source_sha1 '2e97feed81919465a04ccc71e4073313'
+
+  depends_on 'buildessential'
+
+  def self.build
+    system "./configure $XORG_CONFIG"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
