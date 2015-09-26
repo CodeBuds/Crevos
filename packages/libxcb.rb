929058a57acb728186d3a1ad3571423c596031c9
David Smerkous <smerkousdavid@gmail.com>
2015-09-26 16:01:05 -0500

    Update libxcb.rb

--- packages/libxcb.rb
+++ packages/libxcb.rb
@@ -2,8 +2,8 @@ require 'package'
 
 class Libxcb < Package
   version '0.1.1'
-  source_url 'http://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.bz2'
-  source_sha1 '6bf2797445dc6d43e9e4707c082eff9c'
+  source_url 'http://xcb.freedesktop.org/dist/libxcb-1.11.1.tar.bz2'
+  source_sha1 'f97a65e6158775de518ac391935634c2'
 
   depends_on 'buildessential'
 
