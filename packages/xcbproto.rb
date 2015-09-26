bf3dae6fe3a7a6dde90037045c6d97569aa40a36
David Smerkous <smerkousdavid@gmail.com>
2015-09-26 15:58:14 -0500

    Create xcbproto.rb

---
+++ packages/xcbproto.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Xfonts < Package
+  version '0.1.1'
+  source_url 'http://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.bz2'
+  source_sha1 '6bf2797445dc6d43e9e4707c082eff9c'
+
+  depends_on 'buildessential'
+
+  def self.build
+    system "./configure $XORG_CONFIG"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
