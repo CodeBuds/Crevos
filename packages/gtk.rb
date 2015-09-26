124dbb16ecf1504ae1b89ffac523e4cd9a25a2b3
David Smerkous <smerkousdavid@gmail.com>
2015-09-25 23:27:54 -0500

    GRAPHICAL STUFF

--- packages/gtk.rb
+++ packages/gtk.rb
@@ -6,12 +6,12 @@ class Gtk < Package
   source_sha1 'ce617318fe18092383cf6ed5d8c688a95a97f2d4c68481317a6a531e288c26ea'
   
   def self.build
-    system "./configure", "--prefix=/opt/gtk"
+    system "./configure --prefix=/opt/gtk --with-internal-glib --libdir=/usr/local/lib64/ CC=\"gcc -m64\" CFLAGS=\" -fPIC\""
+    system "make"
   end
   
   def self.install
-    system "make"
-    system "make", "install"
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
 end
     
