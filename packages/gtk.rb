7d4309c866e2692484c2c73c9ede1b0193e3ad02
David Smerkous <smerkousdavid@gmail.com>
2015-09-25 23:30:57 -0500

    added dependencies

--- packages/gtk.rb
+++ packages/gtk.rb
@@ -5,6 +5,8 @@ class Gtk < Package
   source_url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.16/gtk+-3.16.0.tar.xz'
   source_sha1 'ce617318fe18092383cf6ed5d8c688a95a97f2d4c68481317a6a531e288c26ea'
   
+  depends_on 'pkgconfig'
+  
   def self.build
     system "./configure --prefix=/opt/gtk --with-internal-glib --libdir=/usr/local/lib64/ CC=\"gcc -m64\" CFLAGS=\" -fPIC\""
     system "make"
