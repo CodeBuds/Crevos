03b90f204abe7aef400f6204097dee3ee2ec42b8
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 17:44:33 -0500

    Update libevdev.rb

--- packages/libevdev.rb
+++ packages/libevdev.rb
@@ -13,6 +13,6 @@ class Libevdev < Package
   end
 
   def self.install
-    system "sudo make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "sudo make", "DESTDIR=#{CREVOS_DEST_DIR}", "install"
   end
 end
