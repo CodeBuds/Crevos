359cf16a93407dbc7add768f4a6faee18800098f
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 17:29:02 -0500

    Update libevdev.rb

--- packages/libevdev.rb
+++ packages/libevdev.rb
@@ -8,11 +8,11 @@ class Libevdev < Package
   depends_on 'buildessential'
 
   def self.build
-    system "./configure $XORG_CONFIG"
-    system "make"
+    system "sudo ./configure $XORG_CONFIG"
+    system "sudo make"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "sudo make", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
 end
