c2dd0111f64e575cd3dce90f07d31327d55ffe18
plunkinguitar <plunkinguitar@gmail.com>
2015-09-27 18:26:03 -0500

    Update pango.rb

--- packages/pango.rb
+++ packages/pango.rb
@@ -2,11 +2,11 @@ require 'package'
 
 class Pango < Package
   version '0.1.1'
-  source_url 'http://ftp.gnome.org/pub/GNOME/sources/pango/1.38/pango-1.38.0.tar.xz'
-  source_sha1 '1d4e75974bad853ee9ac5fc5caee5e7ab235abbd945d51d01f3806e04e7c226c'
+  source_url 'ftp://ftp.gnome.org/pub/gnome/sources/pango/1.36/pango-1.36.8.tar.xz'
+  source_sha1 '217a9a753006275215fa9fa127760ece'
   
   def self.build
-    system "sudo ./configure"
+    system "sudo ./configure --prefix=/usr --sysconfdir=/etc"
     system "sudo make"
   end
   
@@ -15,9 +15,3 @@ class Pango < Package
     #system "sudo pango-querymodules --update-cache"
   end
 end
-#
-#
-#
-#
-#
-#
