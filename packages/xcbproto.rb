3f6f9be1768567413c54633475508879098e2745
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 17:56:52 -0500

    Update xcbproto.rb

--- packages/xcbproto.rb
+++ packages/xcbproto.rb
@@ -1,18 +1,16 @@
 require 'package'
 
-class Xfonts < Package
+class Xproto < Package
   version '0.1.1'
   source_url 'http://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.bz2'
   source_sha1 '6bf2797445dc6d43e9e4707c082eff9c'
 
-  depends_on 'buildessential'
-
   def self.build
-    system "./configure $XORG_CONFIG"
-    system "make"
+    system "sudo ./configure $XORG_CONFIG"
+    system "sudo make"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREVOS_DEST_DIR}", "install"
+    system "sudo make", "DESTDIR=#{CREVOS_DEST_DIR}", "install"
   end
 end
