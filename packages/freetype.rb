822039bb9f9d2bd2482856a203b52678e0e87696
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 16:57:50 -0500

    Update freetype.rb

--- packages/freetype.rb
+++ packages/freetype.rb
@@ -6,11 +6,11 @@ class Freetype < Package
   source_sha1 '5682890cb0267f6671dd3de6eabd3e69'
 
   def self.build
-    system "sed -i  -e \"/AUX.*.gxvalid/s@^# @@" -e "/AUX.*.otvalid/s@^# @@\" modules.cfg && sed -ri -e 's:.*(#.*SUBPIXEL.*) .*:\1:' include/config/ftoption.h && ./configure --prefix=/usr --disable-static"
-    system "make"
+    system "sudo sed -i  -e \"/AUX.*.gxvalid/s@^# @@" -e "/AUX.*.otvalid/s@^# @@\" modules.cfg && sed -ri -e 's:.*(#.*SUBPIXEL.*) .*:\1:' include/config/ftoption.h && ./configure --prefix=/usr --disable-static"
+    system "sudo make"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install && install -v -m755 -d /usr/share/doc/freetype-2.6 && cp -v -R docs/*     /usr/share/doc/freetype-2.6"
+    system "sudo make", "DESTDIR=#{CREW_DEST_DIR}", "install && sudo install -v -m755 -d /usr/share/doc/freetype-2.6 && cp -v -R docs/*     /usr/share/doc/freetype-2.6"
   end
 end
