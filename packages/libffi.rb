9bf1fc404c267de2c14c8c73725b3bebab438208
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 17:40:32 -0500

    Update libffi.rb

--- packages/libffi.rb
+++ packages/libffi.rb
@@ -7,17 +7,13 @@ class Libffi < Package
 
   def self.build
     system "sudo sed -e '/^includesdir/ s/$(libdir).*$/$(includedir)/' \
-    -i include/Makefile.in &&
-
- sudo sed -e '/^includedir/ s/=.*$/=@includedir@/' \
+    -i include/Makefile.in && sudo sed -e '/^includedir/ s/=.*$/=@includedir@/' \
     -e 's/^Cflags: -I${includedir}/Cflags:/' \
-    -i libffi.pc.in        &&
-
-sudo ./configure --prefix=/usr --disable-static"
+    -i libffi.pc.in && sudo ./configure --prefix=/usr --disable-static"
     system "sudo make"
   end
 
   def self.install
-    system "sudo make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "sudo make", "DESTDIR=#{CREVOS_DEST_DIR}", "install"
   end
 end
