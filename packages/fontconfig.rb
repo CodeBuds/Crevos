c294921b7ddead2908f04c56a6df028688a409a7
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 17:28:21 -0500

    Update fontconfig.rb

--- packages/fontconfig.rb
+++ packages/fontconfig.rb
@@ -4,8 +4,7 @@ class Fontconfig < Package
   version '0.1.1'
   source_url 'http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.11.1.tar.bz2'
   source_sha1 '824d000eb737af6e16c826dd3b2d6c90'
-
-  depends_on 'buildessential'
+  
   depends_on 'freetype'
 
   def self.build
