b71eadbb63f6cf6562b7371326d419dbe22b2482
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 18:47:46 -0500

    Update freetype.rb

--- packages/freetype.rb
+++ packages/freetype.rb
@@ -6,7 +6,7 @@ class Freetype < Package
   source_sha1 '5682890cb0267f6671dd3de6eabd3e69'
 
   def self.build
-    system "sudo sed -i  -e \"/AUX.*.gxvalid/s@^# @@"+" -e"+"/AUX.*.otvalid/s@^# @@\" modules.cfg && sed -ri -e 's:.*(#.*SUBPIXEL.*) .*:\1:' include/config/ftoption.h && ./configure --prefix=/usr --disable-static"
+    system "sudo sed -i  -e \"/AUX.*.gxvalid/s@^# @@"+" -e"+"/AUX.*.otvalid/s@^# @@\" modules.cfg && sudo sed -ri -e 's:.*(#.*SUBPIXEL.*) .*:\1:' include/config/ftoption.h && sudo ./configure --prefix=/usr --disable-static"
     system "sudo make"
   end
 
