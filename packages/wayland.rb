a68f06416644f5e3c12edeeba72ac01a706dcc7d
plunkinguitar <plunkinguitar@gmail.com>
2015-09-27 17:35:12 -0500

    Update wayland.rb

--- packages/wayland.rb
+++ packages/wayland.rb
@@ -1,9 +1,11 @@
 require 'package'
 
-class Atk < Package
+class Wayland < Package
   version '0.1.1'
   source_url 'http://wayland.freedesktop.org/releases/wayland-1.8.1.tar.xz'
   source_sha1 '6e877877c3e04cfb865cfcd0733c9ab1'
+  
+  depends_on 'libffi'
   
   def self.build
     system "sudo ./configure --prefix=/usr    \
