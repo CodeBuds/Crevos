88d95f4059a44cde97c803c4538a9ffa65db8f04
David Smerkous <smerkousdavid@gmail.com>
2015-09-27 19:10:43 -0500

    Update allgcc.rb

--- packages/allgcc.rb
+++ packages/allgcc.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class Gccall < Package
+class Allgcc < Package
   version '0.0.1-baseline'
   binary_url ({
     i686: "https://dl.dropboxusercontent.com/s/c06pcge8ogsqfcd/gcc-4.8.1-baseline-chromeos-i686.tar.gz?token_hash=AAFLnE_8iL_lAnGtAAVM5G_sYqejA44jGW8D9r0a8xCjrQ&dl=1",
